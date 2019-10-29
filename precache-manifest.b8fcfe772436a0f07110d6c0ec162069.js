e0c656c716336f5afabe585b775d7989f3ceada8
Chima Chinedum <chinedumchima1000@gmail.com>
2019-10-29 05:14:46 +0100

    Updates

---
+++ precache-manifest.b8fcfe772436a0f07110d6c0ec162069.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a66db89690f1c807f8881eb3de025f16",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "0c1bcef0dab6ce3c20b6",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "4c1de88bb0d3134ec276",
+    "url": "/robofriends/static/css/main.feedfdb7.chunk.css"
+  },
+  {
+    "revision": "0c1bcef0dab6ce3c20b6",
+    "url": "/robofriends/static/js/2.d75c0efc.chunk.js"
+  },
+  {
+    "revision": "4c1de88bb0d3134ec276",
+    "url": "/robofriends/static/js/main.6c60780d.chunk.js"
+  },
+  {
+    "revision": "69ae533dd49bc2dbc7cd",
+    "url": "/robofriends/static/js/runtime-main.9f59819d.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
